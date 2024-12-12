4355c773e8fe3c990a91d6fadd43b445d317aad4
paridhi898 <parirl898@gmail.com>
2024-12-12 22:20:16 +0545

    first commit

---
+++ custom.js
@@ -0,0 +1,12 @@
+// usernamem="paridhi rana"
+// console.log(username)
+let name='Paridhi Rana'
+let email='paridhi123@gmail.com'
+let rollnum='26'
+let address='Dhapasi'
+let phone= '9812349823'
+document.getElementById('name').innerHtML=name
+document.getElementById('email').innerHTML=email
+document.getElementById('rollnum').innerHTML=rollnum
+document.getElementById('address').innerHTML=address
+document.getElementById('phone').innerHTML=phone
